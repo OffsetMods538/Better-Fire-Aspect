5cdde0cc36af554ff8bd1dd4c419d534fd672c0a
Dave-Thomas <71213040+OffsetMonkey538@users.noreply.github.com>
2023-02-07 21:58:26 +0200

    Initial commit

---
+++ src/client/java/top/offsetmonkey538/betterfireaspect/BetterFireAspectClient.java
@@ -0,0 +1,10 @@
+package top.offsetmonkey538.betterfireaspect;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterFireAspectClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/top/offsetmonkey538/betterfireaspect/BetterFireAspect.java
@@ -0,0 +1,16 @@
+package top.offsetmonkey538.betterfireaspect;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterFireAspect implements ModInitializer {
+	public static final String MOD_ID = "better-fire-aspect";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
